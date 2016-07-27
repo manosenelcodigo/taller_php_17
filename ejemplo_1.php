26562737bcbc4954c3d3e26e128d0d9000a6d88c
#manosenelcódigo <peligrosamente@gmail.com>
2016-07-26 21:11:47 -0500

    Add files via upload

---
+++ ejemplo_1.php
@@ -0,0 +1,69 @@
+﻿<?php
+$mensaje='';
+if(isset($_POST["grabar"]) and $_POST["grabar"]=="si")
+{
+    //echo $_FILES["file"]["type"];
+    if(empty($_FILES["file"]["name"]))
+    {
+        $mensaje.='Debe seleccionar un archivo';
+    }else
+    {
+        if($_FILES["file"]["type"]=='image/jpeg' or $_FILES["file"]["type"]=='image/png')
+        {
+            copy($_FILES["file"]["tmp_name"],'public/upload/'.$_FILES["file"]["name"]);
+            $mensaje.='Se ha subido 1 archivo exitosamente';
+        }else
+        {
+            $mensaje.='El formato del archivo no es correcto';
+        }
+    }
+    
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Upload simple</title>
+        <link rel="stylesheet" href="public/css/bootstrap.min.css"  /> 
+    </head>
+    <body>
+    
+        <div class="container">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title">Upload simple</h3>
+              </div>
+              <div class="panel-body">
+                <?php
+                if(!empty($mensaje))
+                {
+                    ?>
+                    <div class="alert alert-danger"><?php echo $mensaje?></div>
+                    <?php
+                }
+                ?>
+                <form action="" method="post" enctype="multipart/form-data">
+                    
+                    <p>
+                        <input type="file" name="file" />
+                    </p>
+                    
+                    <p>
+                    <hr />
+                    <input type="hidden" name="grabar" value="si" />
+                    <input type="submit" value="Enviar" />
+                    </p>
+                    
+                </form>
+                
+                
+              </div>
+            </div>
+        </div>
+        
+    <script src="public/js/jquery-1.10.2.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/funciones.js"></script>
+    </body>
+</html>
---
+++ ejemplo_2.php
@@ -0,0 +1,74 @@
+﻿<?php
+$mensaje='';
+if(isset($_POST["grabar"]) and $_POST["grabar"]=="si")
+{
+    if(empty($_FILES["file"]["name"][0]))
+    {
+        $mensaje.='Debe seleccionar un archivo';
+    }else
+    {
+        $total=count($_FILES["file"]["name"]);
+        $contador=1;
+        for($i=0;$i<$total;$i++)
+        {
+            if($_FILES["file"]["type"][$i]=='image/jpeg' or $_FILES["file"]["type"][$i]=='image/png')
+            {
+                copy($_FILES["file"]["tmp_name"][$i],'public/upload/'.$_FILES["file"]["name"][$i]);
+                $mensaje.='La foto '.$contador.'  se ha subido exitosamente exitosamente<br />';
+            }else
+            {
+                $mensaje.='La foto '.$contador.' no ha podido subirse debido al formato del archivo<br />';
+            }
+            $contador++;
+        }
+    }
+    
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8" />
+        <title>Upload simple</title>
+        <link rel="stylesheet" href="public/css/bootstrap.min.css"  /> 
+    </head>
+    <body>
+    
+        <div class="container">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title">Upload simple</h3>
+              </div>
+              <div class="panel-body">
+                <?php
+                if(!empty($mensaje))
+                {
+                    ?>
+                    <div class="alert alert-danger"><?php echo $mensaje?></div>
+                    <?php
+                }
+                ?>
+                <form action="" method="post" enctype="multipart/form-data">
+                    
+                    <p>
+                        <input type="file" name="file[]" multiple="true" />
+                    </p>
+                    
+                    <p>
+                    <hr />
+                    <input type="hidden" name="grabar" value="si" />
+                    <input type="submit" value="Enviar" />
+                    </p>
+                    
+                </form>
+                
+                
+              </div>
+            </div>
+        </div>
+        
+    <script src="public/js/jquery-1.10.2.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/funciones.js"></script>
+    </body>
+</html>
